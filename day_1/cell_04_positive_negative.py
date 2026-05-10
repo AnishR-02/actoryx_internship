494f13411a5063f1d3ce5c0478850f421ea4003d
AnishR-02 <148347167+AnishR-02@users.noreply.github.com>
2026-05-10 19:55:15 +0530

    Added day 1 python codes

---
+++ day_1/cell_04_positive_negative.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+
+if num1 > 0:
+    print(f"{num1} is positive")
+elif num1 < 0:
+    print(f"{num1} is negative")
+else:
+    print(f"{num1} is equal to 0")
---
+++ day_1/cell_13_prime_check.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+
+if n < 2:
+    print(f"{n} is not a prime number")
+else:
+    is_prime = True
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(f"{n} is a prime number")
+    else:
+        print(f"{n} is not a prime number")
